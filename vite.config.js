4bfb16ca99ee5b1dcf3c0c304fafcb7305aff0c0
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-12 23:40:31 +0200

    Include Quantic News in GitHub Pages build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import { resolve } from 'path'
 export default defineConfig({
   base:'./',
   build:{rollupOptions:{input:{
-    home:resolve(process.cwd(),'index.html'),lab:resolve(process.cwd(),'lab.html'),projects:resolve(process.cwd(),'projects.html'),research:resolve(process.cwd(),'research.html'),team:resolve(process.cwd(),'team.html'),press:resolve(process.cwd(),'press.html'),careers:resolve(process.cwd(),'careers.html'),contact:resolve(process.cwd(),'contact.html')
+    home:resolve(process.cwd(),'index.html'),
+    lab:resolve(process.cwd(),'lab.html'),
+    projects:resolve(process.cwd(),'projects.html'),
+    research:resolve(process.cwd(),'research.html'),
+    team:resolve(process.cwd(),'team.html'),
+    press:resolve(process.cwd(),'press.html'),
+    news:resolve(process.cwd(),'news.html'),
+    careers:resolve(process.cwd(),'careers.html'),
+    contact:resolve(process.cwd(),'contact.html')
   }}}
 })
