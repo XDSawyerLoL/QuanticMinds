6f670b656fb6a802b9ff0b2f8bc110ab4d1a6bcb
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-13 17:37:36 +0200

    Build Solutions page

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+
 export default defineConfig({
   base:'./',
   build:{rollupOptions:{input:{
     home:resolve(process.cwd(),'index.html'),
     lab:resolve(process.cwd(),'lab.html'),
     projects:resolve(process.cwd(),'projects.html'),
+    solutions:resolve(process.cwd(),'solutions.html'),
     research:resolve(process.cwd(),'research.html'),
     team:resolve(process.cwd(),'team.html'),
     press:resolve(process.cwd(),'press.html'),
